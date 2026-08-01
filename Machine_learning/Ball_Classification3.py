b2b2d4d5b8cacec635a1c8190c580bb536a366b7
Prasad Rampure <prasadrampure7@gmail.com>
2026-08-01 23:23:09 +0530

    Add files via upload

---
+++ Machine_learning/Ball_Classification3.py
@@ -0,0 +1,14 @@
+
+def main():
+    print("Ball Classification Case Study")
+
+    Independant = [[35,1],[47,1],[90,0],[48,1],[90,0],[35,1],[92,0],[35,1],[35,1],[35,1],[96,0],[43,1],[110,0],[35,1],[95,0]]
+
+    Dependant = [1,1,2,1,2,1,2,1,1,1,2,1,2,1,2]
+    
+    print("Independent variables are :",Independant)
+
+    print("Dependant varriables  are :",Dependant)
+
+if __name__ == "__main__":
+    main()
