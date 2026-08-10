12fb841bfc269af9353e5b7b0f85addd28ec80d7
Prasad Rampure <prasadrampure7@gmail.com>
2026-08-10 13:53:46 +0530

    Add files via upload

---
+++ Machine_learning/KNNClassifierX.py
@@ -0,0 +1,28 @@
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+
+def main():
+    #Independent
+    X = np.array([
+        [1,2],
+        [2,3],
+        [3,1],
+        [5,6]
+    ])
+
+    # Dependent
+    Y = np.array(["Red","Red","Blue","Blue"])
+
+    new_point = np.array([[3,3]])
+
+    print("Independent variabels are :")
+    print(X)
+
+    print("Dependent variabels are :")
+    print(Y)
+
+    print("Testing point is :")
+    print(new_point)
+    
+if __name__ == "__main__":
+    main()
