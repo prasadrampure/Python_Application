6c719f114a4e268efcc1e3fda29d1d961d1cbf3c
Prasad Rampure <prasadrampure7@gmail.com>
2026-08-25 17:13:09 +0530

    Add files via upload

---
+++ Machine_learning/Ensemble_California_Boosting.py
@@ -0,0 +1,54 @@
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Step 1 : Load the data
+
+df = pd.read_csv("california_housing.csv")
+
+print("Shape of Dataser :",df.shape)
+print("First 5 records :",df.head())
+
+# Step 2 : Separate Features & Labels
+
+X = df.drop("target",axis=1)
+Y = df["target"]
+
+print("Shape of X :",X.shape)
+print("Shape of Y :",Y.shape)
+
+# Step 3 : Split the Dataset for Training & Testing
+
+x_train,x_test,Y_train,Y_test = train_test_split(
+    X,
+    Y,
+    test_size=0.2,
+    random_state=42)
+
+print("spliting Successfully")
+
+# Step 4 : Create the Boosting model
+
+model = GradientBoostingRegressor(
+    n_estimators=100,
+    learning_rate=0.1,
+    max_depth=3,
+    random_state=42
+)
+
+# Step 5 : Train the model
+
+model = model.fit(x_train,Y_train)
+print("model train successfully")
+
+# Step 6 : Test the model
+
+Y_pred = model.predict(x_test)
+print('Testing the model successfully')
+
+# Step 7 : Evaluate the model
+
+print("MSE :",mean_squared_error(Y_test,Y_pred))
+print("R2 :",r2_score(Y_test,Y_pred))
