df02ee7d46d570f37ad29c868fcff822266b9b99
Prasad Rampure <prasadrampure7@gmail.com>
2026-09-14 22:23:53 +0530

    Add files via upload

---
+++ Deep_learning/CNN_Gray_Display.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import numpy as np
+
+img = Image.open("digit_28x28.png")
+img = img.convert("L")
+img = img.resize((28,28))
+
+pixels = np.array(img)
+
+print("Image Size :",pixels.shape)
+
+print('Pixel Values :')
+print(pixels)
+
+# 0     Pure Black
+# 255   Pure White
+# 50    Dark Gray
+# 120   Medium Gray
+# 200   Light Gray
